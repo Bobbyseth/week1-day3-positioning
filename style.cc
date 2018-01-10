264ed81355949f9c5e16dbfc5b2edf4cdb299d3d
Bobbyseth <seth.griggs007@gmail.com>
2018-01-10 11:40:04 -0600

    Adding content

---
+++ style.cc
@@ -0,0 +1,29 @@
+body {
+}
+
+.subject {
+	width: 500px;
+	margin: 0 auto;
+}
+
+.text {
+	margin: o auto;
+}
+
+.text2 {
+	margin: 0 auto;
+}
+
+.fixed {
+	width: auto;
+	height: 100px;
+	background-color: gray;
+	position: fixed;
+}
+
+.relative {
+	width: auto;
+	height: 100px;
+	background-color: gray;
+	position: relative;
+}
